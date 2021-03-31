b2325a560f4552b2410e2f5e7fa37f0583806d5c
MatveenkovNikita <matveenkoff@gmail.com>
2021-04-01 00:53:02 +0300

    without second task

---
+++ LogApp/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  LogApp
+//
+//  Created by Никита  on 31.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var nameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    private var userName = "admin"
+    private let password = "qwerty"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let loginVC = segue.destination as? PresentViewController else { return }
+        loginVC.greeting = nameTF.text
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func goingToGreetingButton() {
+        if userName != nameTF.text || password != passwordTF.text {
+            showAlert(with: "Error!", and: "Enter your correct name and password")
+            nameTF.text = ""
+            passwordTF.text = ""
+        }
+    }
+    @IBAction func alertForgotNameButton() {
+        showAlert(with: "Be careful!", and: "Your name is \(userName)")
+    }
+    
+    @IBAction func alertForgotPasswordButton() {
+        showAlert(with: "Be careful!", and: "Your password is \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        nameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(false)
+        super .touchesBegan(touches, with: event)
+    }
+
+}
+extension LoginViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
---
+++ LogApp/PresentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PresentViewController.swift
+//  LogApp
+//
+//  Created by Никита  on 31.03.2021.
+//
+
+import UIKit
+
+class PresentViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    
+    var greeting: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.text = "Welcome, " + greeting + "!"
+    }
+    
+    
+
+}
